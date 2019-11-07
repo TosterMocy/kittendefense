4d7554918597f25473537004c2f63f781d24bf7b
TosterMocy <tostermocy@gmail.com>
2019-11-08 00:46:00 +0100

    cała gra

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    //zmienna - ile czasu czekać
+    [SerializeField] int timeToWait = 3;
+
+    //numer aktualnej sceny
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //wybiera z SceneManagera aktualnie włączoną scene
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        //jeżeli włączona scena to scena 0 (Splash Screen)
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+
+    }
+
+    //Coroutine - WaitForTime function
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+
+        //załadowanie kolejnej sceny
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
